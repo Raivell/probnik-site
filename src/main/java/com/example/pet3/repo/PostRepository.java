04e8b68c529763dc4af15372c7d27e8acf08fc70
Sergey Slobodyuk <sergei-slobodyk@mail.ru>
2021-08-23 15:29:55 +0500

    первая попытка

---
+++ src/main/java/com/example/pet3/repo/PostRepository.java
@@ -0,0 +1,8 @@
+package com.example.pet3.repo;
+
+import com.example.pet3.models.Post;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository<Post, Long> {
+
+}
